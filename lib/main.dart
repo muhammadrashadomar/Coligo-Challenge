98d5bf5bb53815435cca5dbb739f11786b096df9
muhammad rashad <muhammadrashad2006@gmail.com>
2022-10-23 20:15:45 +0200

    remove comments in main

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,6 @@ import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await EasyLocalization.ensureInitialized();
-
   runApp(const Coligo());
 }
 
